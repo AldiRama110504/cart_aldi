6a4ef707f50ad9bff07f658ed69e684e370c4218
AldiRama110504 <aldi.rama@student.undiksha.ac.id>
2025-09-24 20:53:10 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/cart_aldi/',
+    base: '/cart_shop/',
    plugins: [
      tailwindcss(),
     react()],
