8466967a9b562187c5a303732eb47eb865e17fe2
AldiRama110504 <aldi.rama@student.undiksha.ac.id>
2025-09-24 20:56:58 +0800

    Finalizing files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/cart_shop/',
+    base: '/cart_aldi/',
    plugins: [
      tailwindcss(),
     react()],
